aed680d66fe71e01f3c0d97922cf2d9ca32609c6
Sergio Bernal Guillot <sergioguillot@gmail.com>
2020-06-15 00:41:13 +0200

    build: clean base

--- packages/base/src/index.ts
+++ packages/base/src/index.ts
@@ -1,9 +1,3 @@
-// import { eachLimit } from 'moduleskit';
-
 (async function main(): Promise<void> {
-  // eachLimit([1,2], 1, (value, done) => {
-  //   console.log(value);
-  //   done();
-  // });
   throw new Error('Main not implemented.');
 }());
