26b357aa2ce40d24889766e17fc881213afd3bdd
Sergio Bernal Guillot <sergioguillot@gmail.com>
2020-06-16 02:22:46 +0200

    feat: adding more to moduleskit
    agenda, mongoose, rxjs, sequelize, socketio

---
+++ packages/moduleskit/src/agenda/index.ts
@@ -0,0 +1,1 @@
+export * as agenda from 'agenda';
---
+++ packages/moduleskit/src/socket.io/types.ts
@@ -0,0 +1,27 @@
+import {
+    Adapter,
+    Client,
+    EngineSocket,
+    Handshake,
+    Namespace,
+    Packet,
+    Room,
+    Rooms,
+    Server,
+    ServerOptions,
+    Socket
+} from 'socket.io';
+
+export {
+    Adapter,
+    Client,
+    EngineSocket,
+    Handshake,
+    Namespace,
+    Packet,
+    Room,
+    Rooms,
+    Server,
+    ServerOptions,
+    Socket
+};
